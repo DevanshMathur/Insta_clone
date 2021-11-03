32c04ff84965313295695b64461aa9df54a1c64d
Devansh Mathur <devansh.mathur@unthinkable.co>
2021-11-03 12:02:20 +0530

    all changes

--- lib/src/app/ui/search/search_page.dart
+++ lib/src/app/ui/search/search_page.dart
@@ -12,6 +12,3 @@ class SearchPage extends StatelessWidget {
     );
   }
 }
-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
-
-https://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@github.com/DevanshMathur/Insta_clone_practise.git
